f0cd46286b925b9edf9b7454154c94225243b6ab
sloanstewart <sloanstewartdev@gmail.com>
2017-12-09 14:22:26 -0500

    removed references to Flash in server and templates

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", (req, res) => {
   res
   .status(200)
-  .render('index', { message: req.flash('info') });
+  .render('index');
 });
 
 
