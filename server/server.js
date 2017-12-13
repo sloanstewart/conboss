63dcca2c7ae9d03f213a3a7ecb62c866a64199f0
sloanstewart <sloanstewartdev@gmail.com>
2017-12-12 20:09:21 -0500

    Added User routes and import for auth middleware

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Event} = require('./models/event');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 // mongoose.Promise = global.Promise;
 
@@ -120,8 +121,44 @@ app.patch('/events/:id', (req, res) => {
   })
 });
 
+// USERS =====================
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
 
+  user.save().then(()=>{
+     return user.generateAuthToken();
+  }).then((token) => {
+      res.header('x-auth', token).send(user);
+  }).catch((err)=>{
+      res.status(400).send(err);
+  });
+});
 
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  
+  User.findByCredentials(body.email, body.password).then((user) => {
+     return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+     });
+  }).catch((e) => {
+      res.status(400).send();
+  });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+      res.status(200).send();
+
+  }, () => {
+      res.status(400);
+  });
+});
 
 // 404 for requests to everything that's not specified
 app.use('*', function(req, res) {
