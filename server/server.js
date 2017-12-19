36a779b62494710bc439562c79367dc41151554f
sloanstewart <sloanstewartdev@gmail.com>
2017-12-18 22:06:08 -0500

    Refactored a lot based on airbnb eslint config...

--- server/server.js
+++ server/server.js
@@ -1,20 +1,18 @@
-// import { ObjectID } from '../../../Users/Dude/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/bson';
-
-/*jshint esversion:6*/
+/* jshint esversion:6 */
 require('./config/config');
 
 const morgan = require('morgan');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 
-const {mongoose} = require('./db/mongoose');
-const {Event} = require('./models/event');
-const {User} = require('./models/user');
-const {authenticate} = require('./middleware/authenticate');
+const { mongoose } = require('./db/mongoose');
+const { Event } = require('./models/event');
+const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
-// mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
 const app = express();
 const port = process.env.PORT;
@@ -27,7 +25,7 @@ app.set('view engine', 'ejs');
 // typical config
 app.use(morgan('common'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // handleRequest = (req, res) => {
@@ -35,104 +33,103 @@ app.use(bodyParser.urlencoded({extended: true}));
 // }
 
 // Front end routes
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res
-  .status(200)
-  .render('index');
+    .status(200)
+    .render('index');
 });
 
-app.get("/signup", (req, res) => {
+app.get('/signup', (req, res) => {
   res
-  .status(200)
-  .render('user-new');
+    .status(200)
+    .render('user-new');
 });
 
-app.get("/login", (req, res) => {
+app.get('/login', (req, res) => {
   res
-  .status(200)
-  .render('user-login');
+    .status(200)
+    .render('user-login');
 });
 
-app.get("/dashboard/:id", (req, res) => {
-  let id = req.params.id;
-  
-  if(!ObjectID.isValid(id)) {
+app.get('/dashboard/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-  
+
   User
-  .findById(id)
-  .then((user) => {
-    if (!user) {
-      return res.status(404).send();
-    }
-    return user
-  })
-  .then(user => {
-    const data = user;
-    res
+    .findById(id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      return user;
+    })
+    .then(user => {
+      res
+        .status(200)
+        .render('user-dashboard', { user });
+    }).catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.get('/events', (req, res) => {
+  res
     .status(200)
-    .render('user-dashboard', {user});
-  });
-})
-    
-app.get("/events", (req, res) => {
-  res
-  .status(200)
-  .render('event-all');
+    .render('event-all');
 });
 
-app.get("/event/new", (req, res) => {
+app.get('/event/new', (req, res) => {
   res
-  .status(200)
-  .render('event-new');
+    .status(200)
+    .render('event-new');
 });
 
-app.get("/event/:id", (req, res) => {
+app.get('/event/:id', (req, res) => {
   res
-  .status(200)
-  .render('event-details');
+    .status(200)
+    .render('event-details');
 });
 
-app.get("/event/edit/:id", (req, res) => {
+app.get('/event/edit/:id', (req, res) => {
   res
-  .status(200)
-  .render('event-edit');
+    .status(200)
+    .render('event-edit');
 });
-    
-
-
 
 
 // API: EVENTS =====================
 app.post('/api/events', (req, res) => {
-  let event = new Event({
+  const { id } = req.user; // watch for error trying to find _id
+  const event = new Event({
     title: req.body.title,
     details: req.body.description,
     start: req.body.start,
     end: req.body.end,
-    _creator: req.user._id
+    _creator: id,
   });
 
   event.save().then((doc) => {
     res.send(doc);
   }, (err) => {
     res.status(400).send(err);
-  }); 
+  });
 });
 
 app.get('/api/events', (req, res) => {
   Event.find().then((events) => {
-    res.send({events});
+    res.send({ events });
   }, (err) => {
     res.status(400).send(err);
-  })
+  });
 });
 
 app.get('/api/events/:id', authenticate, (req, res) => {
-  let id = req.params.id;
+  const { id } = req.params;
 
-  if(!ObjectID.isValid(id)) {
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
@@ -144,61 +141,60 @@ app.get('/api/events/:id', authenticate, (req, res) => {
       return res.status(404).send();
     }
 
-    res.send({event});
+    res.send({ event });
   }).catch((err) => {
-    res.status(400).send();
-  })
+    res.status(400).send(err);
+  });
 });
 
 app.delete('/api/events/:id', (req, res) => {
-  let id = req.params.id;
-  
-    if(!ObjectID.isValid(id)) {
+  const { id } = req.params;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('ID invalid');
+  }
+
+  Event.findByIdAndRemove(id).then((event) => {
+    if (!event) {
       return res.status(404).send();
     }
 
-    Event.findByIdAndRemove(id).then((event) => {
-      if(!event) {
-        return res.status(404).send();
-      }
-
-      res.send({event});
-    }).catch((err) => {
-      res.status(400).send();
-    })
+    res.send({ event });
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
 });
 
 app.patch('/api/events/:id', (req, res) => {
-  var id = req.params.id;
-  var body = _.pick(req.body, ['title', 'details', 'start', 'end', 'users']);
+  const { id } = req.params;
+  const body = _.pick(req.body, ['title', 'details', 'start', 'end', 'users']);
 
   if (!ObjectID.isValid(id)) {
-      return res.status(404).send();
+    return res.status(404).send();
   }
 
-  Event.findByIdAndUpdate(id, {$set: body}, {new: true}).then((event) => {
-      if (!event) {
-          return res.status(404).send();
-      }
+  Event.findByIdAndUpdate(id, { $set: body }, { new: true }).then((event) => {
+    if (!event) {
+      return res.status(404).send();
+    }
 
-      res.send({event});
-  }).catch((e) => {
-      res.status(400).send();
-  })
+    res.send({ event });
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
 });
 
 // API: USERS =====================
 app.post('/users', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  var user = new User(body);
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
 
-  user.save().then(()=>{
-     return user.generateAuthToken();
+  user.save().then(() => {
+    user.generateAuthToken();
   }).then((token) => {
-      res.header('x-auth', token)
-      .render(`/dashboard/`);
-  }).catch((err)=>{
-      res.status(400).send(err);
+    res.header('x-auth', token)
+      .render('/dashboard/');
+  }).catch((err) => {
+    res.status(400).send(err);
   });
 });
 
@@ -207,37 +203,36 @@ app.get('/users/me', authenticate, (req, res) => {
 });
 
 app.post('/users/login', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  
+  const body = _.pick(req.body, ['email', 'password']);
+
   User.findByCredentials(body.email, body.password).then((user) => {
-     return user.generateAuthToken().then((token) => {
+    return user.generateAuthToken().then((token) => {
       res.header('x-auth', token)
-      .status(200)
-      .render('user-dashboard', user);
-        // .render(`dashboard`, user);
-     });
-  }).catch((e) => {
-      res.status(400).send();
+        .status(200)
+        .render('user-dashboard', user);
+      // .render(`dashboard`, user);
+    });
+  }).catch((err) => {
+    res.status(400).send(err);
   });
 });
 
 app.delete('/users/me/token', authenticate, (req, res) => {
   req.user.removeToken(req.token).then(() => {
-      res.status(200).send();
-
+    res.status(200).send();
   }, () => {
-      res.status(400);
+    res.status(400);
   });
 });
 
 // 404 for requests to everything that's not specified
-app.use('*', function(req, res) {
+app.use('*', (req, res) => {
   res.status(404)
-  .render('404');
+    .render('404');
 });
 
 app.listen(port, () => {
-  console.log(`Started on port ${port}`)
-})
+  // console.log(`Started on port ${port}`);
+});
 
-module.exports = {app};
+module.exports = { app };
